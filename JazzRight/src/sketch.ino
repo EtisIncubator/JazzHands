89ea90ffa2963c2b53faf5cd9320b3da5150adc5
Leo Lahti <leo.lahti1@gmail.com>
2016-06-02 13:23:59 +0300

    Added defaults and switch

--- JazzRight/src/sketch.ino
+++ JazzRight/src/sketch.ino
@@ -1,25 +1,34 @@
-/*int frequencies[5] = {1046, 1175, 1316,1397, 1568};*/ /*C D E F G*/
+int orig[5] = {1760, 1976, 2093, 2349, 2638}; /*A B C D E*/
 int frequencies[5] = {1760, 1976, 2093, 2349, 2638}; /*A B C D E*/
+void wait(int pin)
+{
+	int pressed;
+	do {
+		pressed = digitalRead(pin);
+	} while(pressed == LOW);
+}
 void setup()
 {
-	Serial.begin(9600);
 	pinMode(13, OUTPUT);
 	for (int i = 2; i < 9; ++i)
 		pinMode(i, INPUT_PULLUP);
 }
 void loop()
 {
-	for (int i = 0; i < 5; ++i)
+	for (int i = 0; i < 5; i++)
+		if (frequencies[i] <= orig[i]/16 )
+			frequencies[i] = orig[i]/8;
+	for (int i = 0; i < 5; i++)
 		if (digitalRead(i + 2) == LOW)
 			tone(13, frequencies[i], 20);
 	if (digitalRead(7) == LOW){
 		for (int i = 0; i < 5; i++)
 			frequencies[i] /= 2;
-		delay(500);
+		wait(7);
 	}
 	else if (digitalRead(8) == LOW){
 		for (int i = 0; i < 5; i++)
 			frequencies[i] *= 2;
-		delay(500);
+		wait(8);
 	}
 }
